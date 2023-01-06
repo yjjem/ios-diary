8e4e510c2c613201c4cee69ad10a085f1ef27545
woong <workplayhard1@naver.com>
2023-01-06 18:49:17 +0900

    feat(DiaryViewController): CLLocationManagerDelegate 채택 및 현재 사용자의 위치의 위도와 경도 받아오도록 설정함.

--- Diary/Controller/DiaryViewController.swift
+++ Diary/Controller/DiaryViewController.swift
@@ -5,6 +5,7 @@
 // 
 
 import UIKit
+import CoreLocation
 
 final class DiaryViewController: UIViewController {
     private var diaryData: [DiaryData] = []
@@ -12,6 +13,8 @@ final class DiaryViewController: UIViewController {
     private var dataSource: UITableViewDiffableDataSource<Section, DiaryData>?
     private var coreDataManager: CoreDataManager = CoreDataManager()
     private var networkManager: NetworkManager = NetworkManager()
+    private var locationManager: CLLocationManager = CLLocationManager()
+    private var location: Location?
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -20,8 +23,9 @@ final class DiaryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
         view.addSubview(tableView)
-        getWeatherData()
         configureDataSource()
         configureSnapshot()
         setTableView()
@@ -34,6 +38,7 @@ final class DiaryViewController: UIViewController {
     }
     
     @IBAction func tapAddBarButtonItem(_ sender: UIBarButtonItem) {
+        getWeatherData() // TODO: detailVC 처리로 변경
         coreDataManager.create()
         diaryData = coreDataManager.fetch()
         let detailViewController = storyboard?.instantiateViewController(identifier: Identifiers.detalViewControllerID) as? DetailViewController ?? DetailViewController()
@@ -42,7 +47,8 @@ final class DiaryViewController: UIViewController {
     }
     
     private func getWeatherData() {
-        let urlComponenets = NetworkURL.weatherData(latitude: "10", longitude: "10")
+        guard let location = location else { return }
+        let urlComponenets = NetworkURL.weatherData(latitude: location.latitude, longitude: location.longitude)
         guard let url = urlComponenets.url else { return }
         networkManager.requestData(url: url, type: OpenWeather.self) { data in
             switch data {
@@ -117,4 +123,65 @@ extension DiaryViewController {
         snapShot.reloadSections([.main])
         dataSource?.apply(snapShot, animatingDifferences: false)
     }
+}
+
+// MARK: - CLLocation Delegate
+
+extension DiaryViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coordinate = locations.last?.coordinate else { return }
+        let latitude = coordinate.latitude.description
+        let longitude = coordinate.longitude.description
+        print(latitude, longitude)
+        location = Location(latitude: latitude, longitude: longitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDivicesLocationAuthorization()
+    }
+    
+    func checkUserDivicesLocationAuthorization() {
+        DispatchQueue.global().async {
+            guard CLLocationManager.locationServicesEnabled() else {
+                self.locationSettingAlert()
+                return
+            }
+        }
+        
+        // 2. 현재, 앱의 허용수준을 체크하여 허용수준에 따라 분기처리한다.
+        let authorizaitonStatus: CLAuthorizationStatus
+        authorizaitonStatus = locationManager.authorizationStatus
+        handleAccordingToAuthorizationStatusLevel(authorizaitonStatus)
+    }
+    
+    func handleAccordingToAuthorizationStatusLevel(_ status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .denied, .restricted:
+            locationSettingAlert()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            print("Default")
+        }
+    }
+    
+    private func locationSettingAlert() {
+        let alert = UIAlertController(title: "위치를 설정해주세요.", message: "설정으로이동", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "네", style: .default) { action in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+            UIApplication.shared.open(url)
+        }
+        let noAction = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(okAction)
+        alert.addAction(noAction)
+        present(alert, animated: true)
+    }
+    
 }
---
+++ Diary/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  Diary
+//
+//  Created by 서현웅 on 2023/01/06.
+//
+
+import Foundation
+
+struct Location {
+    var latitude: String
+    var longitude: String
+    
+}
